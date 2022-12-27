30ec6e4e3968b26e116101f953ee29d15ecb9b1f
Florian Berchtold <florian.berchtold@luminartech.com>
2022-12-27 12:19:07 -0800

    Create nvidia.sh

---
+++ nvidia.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/experimental/$distribution/libnvidia-container.list | \
+         sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+         sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sudo apt-get update
+
+
+curl -sSL get.docker.com | sudo bash
+sudo systemctl --now enable docker
+sudo docker run hello-world
+
+sudo apt-get install -y nvidia-docker2
+sudo systemctl restart docker
+
